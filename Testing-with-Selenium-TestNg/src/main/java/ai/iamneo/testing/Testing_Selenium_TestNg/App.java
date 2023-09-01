31bd7effb50308db81cd5fad3d5c0e951a284581
Iamneo <techadmin@iamneo.ai>
2023-09-01 08:45:43 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/main/java/ai/iamneo/testing/Testing_Selenium_TestNg/App.java
+++ Testing-with-Selenium-TestNg/src/main/java/ai/iamneo/testing/Testing_Selenium_TestNg/App.java
@@ -1,5 +1,14 @@
-package main.java.ai.iamneo.testing.Testing_Selenium_TestNg;
+#package main.java.ai.iamneo.testing.Testing_Selenium_TestNg;
+package ai.iamneo.testing.Testing_Selenium_TestNg;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
 public class App {
-    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
